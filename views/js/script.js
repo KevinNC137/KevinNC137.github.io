35e819f90dfbc926371cfa6c4d8cfd6d2fa5c245
Kevin Nguyen <kevinnguyen@Kevins-MacBook-Pro.local>
2020-07-01 17:37:51 -0400

    adding existing site

---
+++ views/js/script.js
@@ -0,0 +1,27 @@
+$(document).on('scroll', onScroll);
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    console.log(scrollPos);
+
+    if (scrollPos > 280) {
+        $('.nav').removeClass('unfixed');
+        $('.nav').addClass('fixed');
+    } else {
+        $('.nav').removeClass('fixed');
+        $('.nav').addClass('unfixed');
+    }
+
+    $('.nav li a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        
+        if (refElement.position().top <= scrollPos + 400 && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav li').removeClass("active");
+            currLink.parent().addClass('active');
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
